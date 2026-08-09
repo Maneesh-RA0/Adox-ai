2427bcd3ff4972843d50d34f48ec0c12841ff197
myadav78741-crypto <myadav78741@gmail.com>
2026-08-09 12:50:12 +0530

    Fixed missing darkTheme.css import in main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -3,7 +3,6 @@ import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './styles/globals.css'
 import './styles/animations.css'
-import './styles/darkTheme.css'
 
 ReactDOM.createRoot(document.getElementById('app')).render(
     <React.StrictMode>
